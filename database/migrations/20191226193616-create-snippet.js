6054f63e0569355bf6fa002242b5f0c9e5bfc6b0
Chinedu Orie <nedsoftdeveloper@gmail.com>
2019-12-26 21:13:25 +0100

    Set up Eslint

--- database/migrations/20191226193616-create-snippet.js
+++ database/migrations/20191226193616-create-snippet.js
@@ -1,57 +1,51 @@
-'use strict';
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Snippets', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      uid: {
-        type: Sequelize.STRING,
-        type: Sequelize.UUID,
-        defaultValue: Sequelize.UUIDV4
-      },
-      userId: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-      },
-      title: {
-        type: Sequelize.STRING,
-        allowNull: false
-      },
-      description: {
-        type: Sequelize.TEXT,
-        allowNull: true
-      },
-      content: {
-        type: Sequelize.TEXT,
-        allowNull: true
-      },
-      lang: {
-        type: Sequelize.STRING,
-        allowNull: true
-      },
-      sourceUrl: {
-        type: Sequelize.STRING,
-        allowNull: true
-      },
-      tags: {
-        type: Sequelize.ARRAY,
-        allowNull: true
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Snippets');
-  }
+  up: (queryInterface, Sequelize) => queryInterface.createTable('Snippets', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER,
+    },
+    uid: {
+      type: Sequelize.UUID,
+      defaultValue: Sequelize.UUIDV4,
+    },
+    userId: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+    },
+    title: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: Sequelize.TEXT,
+      allowNull: true,
+    },
+    content: {
+      type: Sequelize.TEXT,
+      allowNull: true,
+    },
+    lang: {
+      type: Sequelize.STRING,
+      allowNull: true,
+    },
+    sourceUrl: {
+      type: Sequelize.STRING,
+      allowNull: true,
+    },
+    tags: {
+      type: Sequelize.ARRAY,
+      allowNull: true,
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+  }),
+  down: (queryInterface, Sequelize) => queryInterface.dropTable('Snippets'),
 };
