d741b5f95a1acc63b6e3ce228a82d999e70e491c
Chinedu Orie <nedsoftdeveloper@gmail.com>
2020-04-07 11:44:56 +0100

    Add github column to users

--- database/models/user.js
+++ database/models/user.js
@@ -25,6 +25,10 @@ module.exports = (sequelize, DataTypes) => {
         type: DataTypes.STRING,
         allowNull: true,
       },
+      github: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
       bio: {
         type: DataTypes.TEXT,
         allowNull: true,
